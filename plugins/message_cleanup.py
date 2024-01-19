4895bf8ec6caf88a0928e3dabfa362753df02715
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-19 13:44:23 +0530

    Update message_cleanup.py

--- plugins/message_cleanup.py
+++ plugins/message_cleanup.py
@@ -1,8 +1,5 @@
 import asyncio
-from pyrogram import filters, Client
-from datetime import datetime, timedelta
-
-from bot import Bot
+from pyrogram import filters
 
 async def delete_bot_messages(client, chat_id, message_ids, delay):
     await asyncio.sleep(delay)
@@ -11,10 +8,10 @@ async def delete_bot_messages(client, chat_id, message_ids, delay):
     except Exception as e:
         print(f"Error deleting messages: {e}")
 
-@Bot.on_message(filters.outgoing)
+@Bot.on_message(filters.user(Bot.get_me().id) & ~filters.command("start"))
 async def outgoing_message_handler(client, message):
-    # Set the delay for message deletion (e.g., 5 minutes)
-    delay = 5 * 60  # seconds
+    # Set the delay for message deletion (e.g., 10 seconds for testing)
+    delay = 10  # seconds
 
     # Schedule a task to delete the bot's messages after the specified delay
     asyncio.create_task(delete_bot_messages(client, message.chat.id, [message.message_id], delay))
