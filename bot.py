8ed3f969d3a2f3dee58902f1167efed20e1baaec
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-19 14:19:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,9 +56,8 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
-        self.LOGGER(__name__).info(f""" \n\n       
-\\\xcvbnm,
-                                          """)
+        self.LOGGER(__name__).info(f"""\n\n       
+\\\xcvb""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
